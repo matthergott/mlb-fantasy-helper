d56253de695a9dace6712fa08c7915b54535c44f
matthergott <mchergot@uwaterloo.ca>
2017-03-14 21:59:54 -0400

    Refactor

---
+++ MLBFantasyHelper/app/app-routing.module.ts
@@ -0,0 +1,17 @@
+﻿import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DisplayPlayerComponent } from './frontend/displayPlayer/display-player.component';
+import { HomeComponent } from './frontend/home/home.component';
+
+const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    { path: 'home', component: HomeComponent },
+    { path: 'player/:id', component: DisplayPlayerComponent }
+];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule { }
